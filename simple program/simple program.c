c7429962fb6a977da4e8fc69b84c7f56c42b95dd
ananya-a2006 <ananyapr787@gmail.com>
2025-01-10 00:38:18 +0530

    Add files via upload

---
+++ simple program/simple program.c
@@ -0,0 +1,11 @@
+#include <stdio.h> 
+void main() 
+{ 
+	int a,b,c;
+	printf("This program adds two numbers\n"); 
+	printf("Enter two numbers\n"); 
+	scanf("%d%d",&a,&b); 
+	c=a+b;
+	printf("The sum of %d and %d is %d\n",a,b,c); 
+} 
+
